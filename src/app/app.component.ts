a0d07e37beb42ea3997177136765868c2a723eef
Phạm Hữu Sáng <huusang.dev>
2020-03-10 10:10:25 +0700

    @input and @output

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,12 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'shop';
+
+  loadFeature : string = 'recipes';
+
+  onNavigate(feature : string){
+    this.loadFeature= feature;
+  }
+
+
 }
--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Output, EventEmitter } from '@angular/core';
 import { Recipe } from '../recipe.model';
 
 @Component({
@@ -8,13 +8,19 @@ import { Recipe } from '../recipe.model';
 })
 export class RecipeListComponent implements OnInit {
 
+  @Output() handleRecipeSelected = new EventEmitter<Recipe>();
+
   recipes : Recipe[] = [
     new Recipe('A Test Recipe', 'This is simply a test', 'https://upload.wikimedia.org/wikipedia/commons/1/15/Recipe_logo.jpeg'),
-    new Recipe('A Test Recipe', 'This is simply a test', 'https://upload.wikimedia.org/wikipedia/commons/1/15/Recipe_logo.jpeg')
+    new Recipe('A another Recipe', 'This is simply a A another Recipe', 'https://upload.wikimedia.org/wikipedia/commons/1/15/Recipe_logo.jpeg')
   ]
   constructor() { }
 
   ngOnInit() {
+  }
+
+  onRecipeSelected(recipe :Recipe){
+    this.handleRecipeSelected.emit(recipe);
   }
 
 }
